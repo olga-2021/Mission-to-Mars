232e859e819d26fddf455b92bdbe1ad4c633f788
Olga Postolachi <82691082+olga-2021@users.noreply.github.com>
2021-09-12 21:34:27 -0400

    Add files via upload

---
+++ Mission_to_Mars_Challenge_starter_code.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import Splinter, BeautifulSoup, and Pandas
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# In[2]:
+
+
+# Set the executable path and initialize Splinter
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# ### Visit the NASA Mars News Site
+
+# In[3]:
+
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com/'
+browser.visit(url)
+
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+
+# In[4]:
+
+
+# Convert the browser html to a soup object and then quit the browser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+
+slide_elem = news_soup.select_one('div.list_text')
+
+
+# In[5]:
+
+
+slide_elem.find('div', class_='content_title')
+
+
+# In[6]:
+
+
+# Use the parent element to find the first a tag and save it as `news_title`
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+
+# In[7]:
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
+
+# ### JPL Space Images Featured Image
+
+# In[8]:
+
+
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+
+# In[9]:
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[10]:
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+img_soup
+
+
+# In[11]:
+
+
+# find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+# In[12]:
+
+
+# Use the base url to create an absolute url
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+
+
+# ### Mars Facts
+
+# In[13]:
+
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.head()
+
+
+# In[14]:
+
+
+df.columns=['Description', 'Mars', 'Earth']
+df.set_index('Description', inplace=True)
+df
+
+
+# In[15]:
+
+
+df.to_html()
+
+
+# # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+
+# ### Hemispheres
+
+# In[16]:
+
+
+# 1. Use browser to visit the URL 
+url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars/'
+
+browser.visit(url)
+
+
+# In[17]:
+
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+html = browser.html
+main_page_soup = soup(html, 'html.parser')
+
+# Range total items
+list_range = len(main_page_soup.select("div.item"))
+
+# for loop for each item
+for i in range(list_range):
+
+    # Get A link for the i selected
+    link_image = main_page_soup.select("div.description a")[i].get('href')
+    
+    # Second page opened
+    browser.visit(f'https://astrogeology.usgs.gov{link_image}')
+    
+    # Parse again the new HTML page
+    html = browser.html
+    sample_image_soup = soup(html, 'html.parser')
+    
+    # Save the full .JPG for the selected hemisphere
+    img_url = sample_image_soup.select_one("div.downloads ul li a").get('href')
+    
+    # Save the img_title for the selected hemisphere
+    img_title = sample_image_soup.select_one("h2.title").get_text()
+    
+    # Create a dictionary for the selected hemisphere 
+    hemisphere = {'img_url': img_url,'title': img_title,  }
+    
+    # Append results dict to hemisphere image urls list
+    hemisphere_image_urls.append(hemisphere)
+    
+    # Reset hemisphere
+    hemisphere = {}
+    
+    # Return to main page
+    browser.back()
+
+
+# In[18]:
+
+
+# 4. Print the list that holds the dictionary of each image url and title.
+hemisphere_image_urls
+
+
+# In[19]:
+
+
+# 5. Quit the browser
+browser.quit()
+
+
+# In[ ]:
+
+
+
+
